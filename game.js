3fbe83131d55971c78172d5ef7656b8a15705294
Luciano Pulido <lucianonicolaspulido@gmail.com>
2023-07-26 23:45:34 -0300

    Add round logic and computer decision

--- game.js
+++ game.js
@@ -1,7 +1,21 @@
 function getComputerChoice(){
     let choice = Math.random();
 
-    if(choice < 0.33) return "Rock";
-    else if(choice < 0.66) return "Paper";
-    else return "Scissors";
+    if(choice < 0.33) return "ROCK";
+    else if(choice < 0.66) return "PAPER";
+    else return "SCISSORS";
 }
+
+function playRound(playerSelection, computerSelection){
+    playerSelection = playerSelection.toUpperCase();
+
+    if(computerSelection == playerSelection) return "DRAW"
+    else if(playerSelection < computerSelection) return !(computerSelection == "SCISSORS" && playerSelection == "PAPER") ? "Player WINS" : "Computer WINS";
+    else return !(playerSelection == "SCISSORS" && computerSelection == "PAPER") ? "Computer WINS" : "Player WINS";
+}
+
+let com = getComputerChoice();
+let pla = "Scissors";
+
+console.log(com);
+console.log(playRound(pla, com));
