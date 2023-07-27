374b6e0d2f3915fc49911d4d8f4f4e81e64cd801
Luciano Pulido <lucianonicolaspulido@gmail.com>
2023-07-27 10:11:22 -0300

    Add buttons functionality and refactor logic game

--- game.js
+++ game.js
@@ -8,28 +8,59 @@ function getComputerChoice(){
 
 function playRound(playerSelection, computerSelection){
     playerSelection = playerSelection.toUpperCase();
+    
+    let selections = document.querySelectorAll(".roundSelection");
+
+    selections[0].textContent = playerSelection;
+    selections[1].textContent = computerSelection;
 
     if(computerSelection == playerSelection) return "DRAW"
     else if(playerSelection < computerSelection) return !(computerSelection == "SCISSORS" && playerSelection == "PAPER") ? "Player WINS" : "Computer WINS";
     else return !(playerSelection == "SCISSORS" && computerSelection == "PAPER") ? "Computer WINS" : "Player WINS";
 }
 
-function game(){
-    let computerScore = 0;
-    let playerScore = 0;
-    
-    while(computerScore < 3 && playerScore < 3){
-        let computerSelection = getComputerChoice()
-        let roundResult = playRound(prompt("Write the option you want"), computerSelection)
-        
-        if(roundResult == "Player WINS") playerScore++;
-        else if(roundResult == "Computer WINS") computerScore++;
+function game(roundResult){
+          
+    if(roundResult == "Player WINS") playerScore++;
+    else if(roundResult == "Computer WINS") computerScore++;
 
-        console.log(computerSelection);
-        console.log(roundResult);
-        console.log(`--Scores--\nComputer: ${computerScore}\nPlayer: ${playerScore}`);
+    let scores = document.querySelectorAll(".score");
+
+    scores[0].textContent = playerScore;
+    scores[1].textContent = computerScore;
+
+    if(playerScore == 3 || computerScore == 3){
+        winMessageContainer.classList.toggle('notVisible');
+        optionsContainer.classList.toggle('notVisible');
+
+        if(playerScore == 3) winMessageContainer.childNodes[1].textContent = "PLAYER wins!";
+        else winMessageContainer.childNodes[1].textContent = "COMPUTER wins!";
     }
-
-    if(playerScore == 3) return "Player won the bo5"
-    else return "Computer won the bo5"
 }
+
+function resetGame(){
+    computerScore = 0;
+    playerScore = 0;
+
+    winMessageContainer.classList.toggle('notVisible');
+    optionsContainer.classList.toggle('notVisible');
+
+    let scores = document.querySelectorAll(".score");
+
+    scores[0].textContent = playerScore;
+    scores[1].textContent = computerScore;
+}
+
+let winMessageContainer = document.querySelector("#winMessageContainer");
+let options = document.querySelectorAll(".option");
+let optionsContainer = document.querySelector("#optionsContainer");
+let playAgainButton = document.querySelector("#playAgainButton");
+
+let computerScore = 0;
+let playerScore = 0;
+
+playAgainButton.addEventListener("click", resetGame);
+
+options.forEach(element => {
+    element.addEventListener('click', () => game(playRound(element.textContent, getComputerChoice())));
+});
